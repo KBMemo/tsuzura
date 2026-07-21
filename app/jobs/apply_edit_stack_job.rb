8ce3b53dcc6b2b7600e41a33596810d5ca196be7
knb <kensei@artifarm.com>
2026-07-22 05:55:36 +0900

    Isolate media background job queue

--- app/jobs/apply_edit_stack_job.rb
+++ app/jobs/apply_edit_stack_job.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ApplyEditStackJob < ApplicationJob
-  queue_as :default
+  queue_as :kbmemo_media
 
   def perform(media_item_id)
     item = MediaItem.find_by(id: media_item_id)
---
+++ test/jobs/queue_isolation_test.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class QueueIsolationTest < ActiveSupport::TestCase
+  test "media jobs use the media-only queue" do
+    job_classes = [
+      ApplyEditStackJob,
+      GenerateWebVariantJob
+    ]
+
+    assert_equal ["kbmemo_media"], job_classes.map(&:queue_name).uniq
+  end
+end
