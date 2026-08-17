eab3d2ec50c72a133f802ecfe9c44c264387fdee
knb <kensei@artifarm.com>
2026-08-18 08:18:13 +0900

    Configure Talisker development ports

--- config/puma.rb
+++ config/puma.rb
@@ -28,8 +28,9 @@
 threads_count = ENV.fetch("RAILS_MAX_THREADS", 3)
 threads threads_count, threads_count
 
-# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-port ENV.fetch("PORT", 3000)
+# Listen on loopback by default. A reverse proxy is the public entrypoint.
+bind_host = ENV.fetch("BINDING", "127.0.0.1")
+bind "tcp://#{bind_host}:#{ENV.fetch("PORT", 3000)}"
 
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
